479786e5d270677d8f81bdeee38f72dfb162ca5a
NazcaLiu <natsuka724@gmail.com>
2017-12-06 12:02:50 +0800

    Luuuuuuuuuunch

---
+++ src/main/java/com/github/NazcaLiu/Claaaaass.java
@@ -0,0 +1,16 @@
+package com.github.NazcaLiu;
+
+public class Claaaaass {
+    private int number = 1;
+
+
+    public int toZero(int number){
+        if(number != 0) {
+            number = 0;
+            return number;
+        }
+        else{
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/com/github/NazcaLiu/ClaaaaassTest.java
@@ -0,0 +1,12 @@
+package com.github.NazcaLiu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClaaaaassTest {
+    @Test
+    public void toZero() throws Exception {
+    }
+
+}
